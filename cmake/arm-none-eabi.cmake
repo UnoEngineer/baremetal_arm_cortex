c1f1f3e43f7fe187d7f51619c848c381ac34f933
Alex Fernandes <afernandes@unikey.com>
2019-06-23 21:18:29 -0400

    Change target to M4 and add paths for compiler so cmake is happy

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -1,8 +1,8 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_C_COMPILER /home/alex/.embedded-tools/gcc-arm-none-eabi-5_3-2016q1/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER /home/alex/.embedded-tools/gcc-arm-none-eabi-5_3-2016q1/bin/arm-none-eabi-g++)
 set(CMAKE_C_COMPILER_ID GNU)
 set(CMAKE_CXX_COMPILER_ID GNU)
 set(CMAKE_C_COMPILER_FORCED TRUE)
